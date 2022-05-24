bb1b6fc34cc55c8dd21b99618e818976b07e5648
Jeremy <xyliang6@terpmail.umd.edu>
2022-05-23 20:50:56 -0400

    newest

--- src/App.js
+++ src/App.js
@@ -1,10 +1,66 @@
-import React from 'react';
+import {useEffect, useState} from 'react';
 
+import MovieCard from './MovieCard';
 
-// e8fbac96
+import './App.css';
+import SearchIcon from './search.svg';
+const API_URL = 'http://www.omdbapi.com?apikey=e8fbac96'
+
+const movie1 = {
+    "Title": "Italian Spiderman",
+    "Year": "2007",
+    "imdbID": "tt2705436",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BYjFhN2RjZTctMzA2Ni00NzE2LWJmYjMtNDAyYTllOTkyMmY3XkEyXkFqcGdeQXVyNTA0OTU0OTQ@._V1_SX300.jpg"
+
+}
+
 const App = () => {
+
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const searchMoives = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        setMovies(data.Search);
+    }
+
+    useEffect(() => {
+        searchMoives('Spiderman');
+    }, []);
+
     return (
-        <h1>App</h1>
+        <div className="app">
+            <h1>MovieLand</h1>
+
+            <div className="search">
+                <input 
+                    placeholder="Search for movies"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <img 
+                    src = {SearchIcon}
+                    alt = "search"
+                    onClick={() => searchMoives(searchTerm)}
+                />
+            </div>
+
+            {movies?.length > 0
+                ? (
+                    <div className="container">
+                        {movies.map((movie) => (
+                            <MovieCard movie={movie}/>
+                        ))}
+                    </div>
+                ) : (
+                    <div className="empty">
+                        <h2>No movies found!</h2>
+                    </div>
+                )}
+        </div>
     );
 }
 
